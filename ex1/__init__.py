550f329ff1be93b7fb65b8e1ba163e759397db5f
vasilyevairenav <vasilyevairenav@gmail.com>
2022-02-08 09:47:18 +0300

    feat: ex1 solved

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -4,4 +4,28 @@ def build_roles_tree(mapping):
     :return: дерево ролей
     """
     # put your code here
-    pass
+    categories = mapping["categories"]
+    roles = mapping['roles']
+    sorted_sequences = mapping['categoryIdsSorted']
+
+    sorted_categories = [categories[ctgr] for ctgr in sorted_sequences]
+    roles_tree = [new_category(ctgr, roles) for ctgr in sorted_categories]
+    return {'categories': roles_tree}
+
+def find_role(role_id, roles):
+    """
+    :param role_id: key id in roles dictionary
+    :param roles: roles dictionary
+    """
+    role = roles[role_id]
+    return {'id': role['id'], 'text': role['name']}
+
+def new_category(catergory, roles):
+    """
+    :param category: category object
+    :param roles: roles dictionary
+    """
+    new_id = 'category-' + catergory['id']
+    text = catergory['name']
+    items = [find_role(role, roles) for role in catergory['roleIds']]
+    return {'id': new_id, 'text': text, 'items': items}
